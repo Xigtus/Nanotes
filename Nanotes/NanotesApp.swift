84b3099525870d9995c3cf19c086b49e967fb374
Gusti R. Fajar <gustirizkyfajar@gmail.com>
2024-07-10 09:11:28 +0700

    Initial Commit

---
+++ Nanotes/NanotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NanotesApp.swift
+//  Nanotes
+//
+//  Created by Gusti Rizky Fajar on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct NanotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
