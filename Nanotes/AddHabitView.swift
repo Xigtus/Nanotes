a8e6e627633c02ef7e926768e31e0e860eb6d48b
Gusti R. Fajar <gustirizkyfajar@gmail.com>
2024-07-11 22:15:32 +0700

    Adds: - Habit tracking notes page - Add habit page - Function to add habit - Function to delete habit - Function to search habit

---
+++ Nanotes/AddHabitView.swift
@@ -0,0 +1,18 @@
+//
+//  AddHabitView.swift
+//  Nanotes
+//
+//  Created by Gusti Rizky Fajar on 11/07/24.
+//
+
+import SwiftUI
+
+struct AddHabitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddHabitView()
+}
---
+++ Nanotes/HabitModel.swift
@@ -0,0 +1,8 @@
+//
+//  HabitModel.swift
+//  Nanotes
+//
+//  Created by Gusti Rizky Fajar on 11/07/24.
+//
+
+import Foundation
